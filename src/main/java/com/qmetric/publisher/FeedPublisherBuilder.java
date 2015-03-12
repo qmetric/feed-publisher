a494d3e33dfd368948b96df025777aa4784c3624
luke I am your father <wbuckley@qmetric.co.uk>
2015-03-12 12:43:10 +0000

    add authentication

--- src/main/java/com/qmetric/publisher/FeedPublisherBuilder.java
+++ src/main/java/com/qmetric/publisher/FeedPublisherBuilder.java
@@ -4,10 +4,14 @@ import com.github.rholder.retry.Retryer;
 import com.github.rholder.retry.RetryerBuilder;
 import com.github.rholder.retry.StopStrategies;
 import com.github.rholder.retry.WaitStrategies;
+import com.squareup.okhttp.Authenticator;
+import com.squareup.okhttp.Credentials;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
+import java.net.Proxy;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -45,5 +49,25 @@ public class FeedPublisherBuilder<T>
         }
 
         return new FeedPublisher<>(client, feedUrl, contentMaker, retryer);
+    }
+
+    public FeedPublisherBuilder<T> addAuthentication(final String user, final String password)
+    {
+        client.setAuthenticator(new Authenticator()
+        {
+            @Override public Request authenticate(final Proxy proxy, final Response response) throws IOException
+            {
+                String credential = Credentials.basic(user, password);
+                return response.request().newBuilder()
+                        .header("Authorization", credential)
+                        .build();
+            }
+
+            @Override public Request authenticateProxy(final Proxy proxy, final Response response) throws IOException
+            {
+                return null;
+            }
+        });
+        return this;
     }
 }
